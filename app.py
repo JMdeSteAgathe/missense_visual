861986c664548a02343976a0dff8ba6d29dd9253
JMdeSteAgathe <99203649+JMdeSteAgathe@users.noreply.github.com>
2026-01-29 09:59:42 +0100

    Update VCF file paths to fully annotated versions

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ import json
 
 TARGET_GENE = "SCN1A"
 variant = "NM_001165963.4(SCN1A):c.1060G>C"
-VCF_FILE = "/path/to/gnomad_ms.BCSQ.PASS.REVEL.am.mpc2.nochr.cadd.vcf.gz"
-CLINVAR_VCF = "/path/to/clinvar_plp_ms.BCSQ.cadd.revel.am.mpc2.nochr.vcf.gz"
+
+VCF_FILE = "/path/to/gnomad_ms.fully_annotated.vcf.gz"
+CLINVAR_VCF = "/path/to/clinvar_plp_ms.fully_annotated.vcf.gz"
 GENE_COORD = "/path/to/gene_coord.csv.gz"
 
 # first, the gene coordinates
